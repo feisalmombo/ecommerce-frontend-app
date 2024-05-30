8eff1b2ae294919826cb8625b89dca16f08965f2
feisalmombo <feisalmombo29@gmail.com>
2024-05-30 13:34:34 +0300

    WIP

--- src/components/PageNotFound.vue
+++ src/components/PageNotFound.vue
@@ -4,7 +4,7 @@
         <div class="card" align="center">
             <div class="card-header">Page Not Found</div>
             <div class="card-body">
-                <h1>404 Page</h1>
+                <h1>404 Not Found</h1>
             </div>
         </div>
     </div>
--- src/components/ProductDetails.vue
+++ src/components/ProductDetails.vue
@@ -1,41 +1,28 @@
 <template>
-    <!-- Product details container -->
-    <!-- Use Vue's 'v-if' directive to render this container only if 'product' is truthy -->
-    <!--This is like a magic switch. If we have a toy car (product details), the stand lights up and shows the toy. If not, it stays empty.-->
     <div v-if="product" class="product-details">
-      <!-- Display the product name -->
       <h2 class="product-title">{{ product.name }}</h2>
-      <!-- Display the product description -->
       <p class="product-description">{{ product.description }}</p>
-      <!-- Display the product price -->
-      <p class="product-price">Price: ${{ product.price }}</p>
+      <p class="product-price">Price: TZS{{ product.price }}</p>
     </div>
   </template>
   
   <script>
-  import axios from '@/axios';  // Import the axios instance
+
+  import axios from '@/axios'; 
   
   export default {
-    // Component data
     data() {
       return {
-        // Initialize product as null
-        product: null // This means our box is empty to start with
-        // it's empty because we haven't picked which toy car we want to display.
+        product: null 
       };
     },
-    // Lifecycle hook that is called after the instance has been mounted
-    async mounted() { // the component being ready and visible in the browser for the first time.then 
-        // we immediately fetch and display the product details.(after that component can update and re-render as many times as needed based on data changes or other interactions.)
-      // Get the product ID from the route params
-      const productId = this.$route.params.id; // This is like a magic number that tells us which toy car we want. Maybe it's toy car number 5 or number 10
+
+    async mounted() { 
+      const productId = this.$route.params.id; 
       try {
-        // Make a GET request to the API to fetch the product data
-        const response = await axios.get(`/products/${productId}`); // our magic remote goes to the toy store and asks, 'Hey, can you tell me about toy car number 5?
-        // Update the product data with the data from the API
-        this.product = response.data; // Once the toy store tells us about the toy car, we put that toy car in our special 'product' box. Now, our display stand lights up and shows off the toy car!"
+        const response = await axios.get(`/products/${productId}`);
+        this.product = response.data; 
       } catch (error) {
-        // If an error occurs, log it to the console
         console.error("An error occurred while fetching the product:", error);
       }
     }
@@ -43,26 +30,26 @@
   </script>
   
   <style scoped>
-  .product-details {
-    max-width: 600px;
-    margin: 20px auto;
-    padding: 20px;
-    background-color: #f9f9f9;
-    border-radius: 8px;
-  }
-  
-  .product-title {
-    margin-bottom: 20px;
-    font-size: 2em;
-  }
-  
-  .product-description {
-    font-size: 1.2em;
-  }
-  
-  .product-price {
-    margin-top: 20px;
-    font-size: 1.2em;
-    font-weight: bold;
-  }
+    .product-details {
+      max-width: 600px;
+      margin: 20px auto;
+      padding: 20px;
+      background-color: #f9f9f9;
+      border-radius: 8px;
+    }
+    
+    .product-title {
+      margin-bottom: 20px;
+      font-size: 2em;
+    }
+    
+    .product-description {
+      font-size: 1.2em;
+    }
+    
+    .product-price {
+      margin-top: 20px;
+      font-size: 1.2em;
+      font-weight: bold;
+    }
   </style>
